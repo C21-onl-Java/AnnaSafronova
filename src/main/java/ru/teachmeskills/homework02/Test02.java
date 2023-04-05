c909d0bebfab24ccebb31e41e439dca077557916
AnnaSfronova <8502423@mail.ru>
2023-04-05 22:08:16 +0300

    comment

--- src/main/java/ru/teachmeskills/homework02/Test02.java
+++ src/main/java/ru/teachmeskills/homework02/Test02.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class Test02 {
 
     public static void main(String[] args) {
+
         System.out.print("Введите имя: ");
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         System.out.println("Hello, " + text);
+
     }
 }
